fd70b2142b295a9a85a06ff7b9d66042e60a30ba
Mon1ca0 <149294834+Mon1ca0@users.noreply.github.com>
2023-11-05 19:29:23 +0530

    Adding source file

---
+++ shellscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the name for the Docker container
+CONTAINER_NAME="nodejs-app"
+
+# Check if a container with the specified name is running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    # Stop the running container
+    docker stop $CONTAINER_NAME
+
+    # Remove the stopped container
+    docker rm $CONTAINER_NAME
+
+    echo "Stopped and removed the existing container: $CONTAINER_NAME"
+fi
+
+# Build a new Docker image from the Dockerfile
+docker build -t nodejs-app-image .
+
+# Run a new container from the image
+docker run -d -p 5000:5000 --name $CONTAINER_NAME nodejs-app-image
+
+echo "New container deployed: $CONTAINER_NAME"
